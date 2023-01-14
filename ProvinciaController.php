4272f3522802e69b3e33ae2bf9428551b6752075
Mateo Martínez <al036366@ieslaencanta.com>
2023-01-14 18:36:11 +0100

    Pongo la extensión php a provincia controller

---
+++ ProvinciaController.php
@@ -0,0 +1,64 @@
+<?php
+class ProvinciaController
+{
+    private $connection;
+    public static $KEY = "provincia";
+    function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+    //guardar item, cierto si se ha podido insertar
+    function save(Provincia $item):bool
+    {
+        $this->connection->hset(ProvinciaController::$KEY, $item->getId(), json_encode($item));
+        $tempo = $this->connection->hget(ProvinciaController::$KEY, $item->getId());
+        if ($tempo != null)
+            return true;
+        else
+            return false;
+    }
+    //borra el elemento
+    function remove(int $id): bool
+    {
+        $tempo = $this->connection->hdel(ProvinciaController::$KEY, $id);
+        if ($tempo != null)
+            return
+                true;
+        else
+            return false;
+    }
+
+    function getAll(): ?array
+    {
+        $items=null;
+        
+        $elements=$this->connection->hgetAll(ProvinciaController::$KEY);
+        if($elements!=null){
+            $items= array();
+            foreach ($elements as $json_text){
+                $tempo=new Provincia();
+                $tempo->loadfromJSON($json_text);
+                array_push($items,$tempo);
+            }
+        }
+        return $items;
+    }
+    function getById(int $id): ?Provincia
+    {
+        $item = null;
+        $json_text = $this->connection->hget(ProvinciaController::$KEY, $id);
+        if ($json_text != null) {
+            $item = new Provincia();
+            $item->loadfromJSON($json_text);
+        }
+        return $item;        
+    }
+    // Falta por hacer este método y el de findLocalidad
+/*     function getAllLocalidades():?array
+    {
+        $item = null;
+        $elements=$this->connection->hgetAll(ProvinciaController::$KEY);
+
+
+    }
+ */}
